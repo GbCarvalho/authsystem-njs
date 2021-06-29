30cb83862904cc81da9daf9bcc7a6879a80605f3
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-06-29 16:39:10 -0300

    Cookie sync and saving token data at every load of the application

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from "next/router";
 import { api } from "../services/api";
-import { setCookie } from "nookies";
+import { parseCookies, setCookie } from "nookies";
 
 type User = {
   email: string;
@@ -29,6 +29,18 @@ export const AuthContext = createContext({} as AuthContextData);
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "authsys.token": token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
