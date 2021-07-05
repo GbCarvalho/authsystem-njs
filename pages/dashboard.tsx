c2756380740103212938fad50bbc454c9a27f146
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-07-05 17:21:11 -0300

    Using high order function to ensure authentication at certain routes

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,12 +6,9 @@ export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    api
-      .get("/me")
-      .then((response) => console.log(response.data))
-      .catch((error) => {
-        console.error(error);
-      });
+    api.get("/me").catch((error) => {
+      console.error(error);
+    });
   }, []);
 
   return <h1>Dashboard: {user?.email}</h1>;
---
+++ utils/withSSRGuest.ts
@@ -0,0 +1,24 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+export async function withSSRGuest<P>(fn: GetServerSideProps<P>) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+
+    if (cookies["authsys.token"]) {
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    }
+    return await fn(ctx);
+  };
+}
