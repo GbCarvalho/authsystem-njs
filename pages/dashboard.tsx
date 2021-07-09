fd425d9d35a0e115947f7ee1e05e5d615a7b809e
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-07-09 17:52:11 -0300

    Using the same principle as withSSRGuest but now verifying if the person isn't aunthenticated

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { api } from "../services/api";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
@@ -13,3 +14,9 @@ export default function Dashboard() {
 
   return <h1>Dashboard: {user?.email}</h1>;
 }
+
+export const getServerSideProps = withSSRAuth(async () => {
+  return {
+    props: {},
+  };
+});
---
+++ utils/withSSRAuth.ts
@@ -0,0 +1,24 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+export async function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+
+    if (!cookies["authsys.token"]) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+    return await fn(ctx);
+  };
+}
