c5bd5ffa0b2962c1eeeb577c075c520327989068
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-07-27 15:19:58 -0300

    Made the certification of the roles and permissions that one user can access

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
@@ -7,13 +8,22 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
+  const userCanSeeMetrics = useCan({
+    roles: ["administrator", "editor"],
+  });
+
   useEffect(() => {
     api.get("/me").catch((error) => {
       console.error(error);
     });
   }, []);
 
-  return <h1>Dashboard: {user?.email}</h1>;
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
